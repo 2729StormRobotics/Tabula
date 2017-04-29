58d7206670aa987bae6de40f5b15ba9e8d28357a
atf1999 <torth212@gmail.com>
2017-04-29 18:16:39 -0400

    added bootstrap, reorganized a little, made the background sexy

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module("dashboard").controller('DashboardController', function($scope){
-    console.log("hello");
-}).directive("motorSpeed", function(){
+angular.module('dashboard').controller('DashboardController', function($scope){
+    console.log('hello');
+}).directive('motorSpeed', function(){
     return{
-        restrict: "E",
-        scope: {ntKey: "@", name: "@"},
-        template: "<p>{{ name }}: {{ textCont }}</p>",
+        restrict: 'E',
+        scope: {ntKey: '@', name: '@'},
+        templateUrl: 'dashboard/templates/motor.html',
         link: function($scope, $element, $attrs){
             NetworkTables.addKeyListener($attrs.ntKey, function(key, value, isNew){
                 $scope.$apply(function(){
